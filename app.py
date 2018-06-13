6ae5bbfa1e29a885665402778d1deb920108bd2c
Pratham Doshi <pratham.doshi95@gmail.com>
2018-06-13 12:59:57 -0700

    test

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    text = 'Hi My name is pratham'
+    return render_template('index.html',text = text)
 
 if __name__ == "__main__":
     app.run(debug=True)
